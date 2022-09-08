770991ed6f5b38efca25ad024cbc1b813e052fe5
kelvinmudanya <kelvinmudanya@gmail.com>
2022-09-08 03:07:14 -0700

    debug

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * Main entry point
- * Returns zero for sucess
+ * main - Entry point
+ *  
+ * Return: Always 0 (Success)
  */
- 
- int main (void){
-	 printf("with proper grammar, but the outcome is a piece of art,\n");
-	 return(0);
- }
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
