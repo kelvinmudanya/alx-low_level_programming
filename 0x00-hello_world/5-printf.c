7dc01f3711a24b05462573edb66e6c848ffd9245
kelvinmudanya <kelvinmudanya@gmail.com>
2022-09-08 02:46:35 -0700

    added mand files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * Main entry point
+ * Returns zero for sucess
+ */
+ 
+ int main (void){
+	 printf("with proper grammar, but the outcome is a piece of art,\n");
+	 return(0);
+ }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void){
+	printf("Size of char: %d bytes(s)\n", sizeof(char));
+	printf("Size of int: %d bytes(s)\n", sizeof(int));
+	printf("Size of long: %d bytes(s)\n", sizeof(long)); 
+	printf("Size of long long int: %d bytes(s)\n", sizeof(long long int)); 
+	printf("Size of float: %d bytes(s)\n", sizeof(float));
+
+	return(0);
+}	
