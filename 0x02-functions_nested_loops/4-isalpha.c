f0ae77b8f10451621a538e74096540b87b06f36c
kelvinmudanya <kelvinmudanya@gmail.com>
2022-09-13 09:51:26 -0700

    check on isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
- /**
-  * _isalpha - Checks if a character is alphabetic.
-  * @c: The character to be checked.
-  * 
-  * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
-  */
- int _isalpha(int c)
+/**
+ * _isalpha - check the code for Holberton School students.
+ * @c: letter being tested
+ * Return: Always 0
+ */
+
+int _isalpha(int c)
 {
-		if ((c >= 'a' && c <= 'z') ||
-					    (c >= 'A' && c <= 'Z'))
-					return (1);
-			else
-						return (0);
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
 }
